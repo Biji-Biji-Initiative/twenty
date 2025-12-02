695a3962d196d2f5318a2d6dc647ccb85fd9ae06
neo773 <62795688+neo773@users.noreply.github.com>
2025-12-03 02:58:51 +0530

    wip

--- packages/twenty-server/src/modules/messaging/message-import-manager/drivers/imap/services/imap-message-text-extractor.service.ts
+++ packages/twenty-server/src/modules/messaging/message-import-manager/drivers/imap/services/imap-message-text-extractor.service.ts
@@ -28,8 +28,6 @@ export class ImapMessageTextExtractorService {
     if (parsed.html) {
       const sanitizedHtml = this.purify.sanitize(parsed.html);
 
-      this.jsdomInstance.window.document.documentElement.innerHTML = `<html><body>${sanitizedHtml}</body></html>`;
-
       const cleanedHtml = planer.extractFromHtml(
         sanitizedHtml,
         this.jsdomInstance.window.document,
